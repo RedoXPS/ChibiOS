fa30bf1827829aeab078ead8cbf6e415f8809e05
RedoXPS <dev@redox.ws>
2015-01-29 17:03:48 +0100

    Add i2c_slave

---
+++ custom/KINETIS/K20x/i2c_slave/i2c_slave_lld.c
@@ -0,0 +1,388 @@
+/*
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    i2c_slave_lld.c
+ * @brief   I2C Slave subsystem low level driver source.
+ *
+ * @addtogroup I2C
+ * @{
+ */
+#include "hal.h"
+
+#if HAL_USE_I2C_SLAVE || defined(__DOXYGEN__)
+
+#include "i2c_slave.h"
+
+static uint8_t i2c_txbuf[4] = {0x00,0x00,0x00,0x00};
+static uint8_t i2c_rxbuf[4] = {0x00,0x00,0x00,0x00};
+/*===========================================================================*/
+/* Driver local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   I2C1 driver identifier.
+ */
+#if KINETIS_I2C_SLAVE_USE_I2C0 || defined(__DOXYGEN__)
+I2CSlaveDriver I2CSD1;
+#endif
+/**
+ * @brief   I2C2 driver identifier.
+ */
+#if KINETIS_I2C_SLAVE_USE_I2C1 || defined(__DOXYGEN__)
+I2CSlaveDriver I2CSD2;
+#endif
+
+/*===========================================================================*/
+/* Driver local variables and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local functions.                                                   */
+/*===========================================================================*/
+
+void slave_config_frequency(I2CSlaveDriver *i2cp) {
+
+  /* Each index in the table corresponds to a a frequency
+   * divider used to generate the SCL clock from the main
+   * system clock.
+   */
+  uint16_t icr_table[] = {
+    /* 0x00 - 0x0F */
+    20,22,24,26,28,30,34,40,28,32,36,40,44,48,56,68,
+    /* 0x10 - 0x1F */
+    48,56,64,72,80,88,104,128,80,96,112,128,144,160,192,240,
+    /* 0x20 - 0x2F */
+    160,192,224,256,288,320,384,480,320,384,448,512,576,640,768,960,
+    /* 0x30 - 0x3F */
+    640,768,896,1024,1152,1280,1536,1920,1280,1536,1792,2048,2304,2560,3072,3840,
+  };
+  uint16_t scl_stop_table[] = {
+    /* 0x00 - 0x0F */
+    11,12,13,14,15,16,18,21,15,17,19,21,23,25,29,35,
+    /* 0x10 - 0x1F */
+    25,29,33,37,41,45,53,65,41,49,57,65,73,81,97,121,
+    /* 0x20 - 0x2F */
+    81,97,113,129,145,161,193,241,161,193,225,257,289,321,384,481,
+    /* 0x30 - 0x3F */
+    321,385,449,513,577,641,769,961,641,769,897,1024,1153,1284,1537,1921
+  };
+
+  int length = sizeof(icr_table) / sizeof(icr_table[0]);
+  uint16_t divisor;
+  uint8_t i = 0, index = 0;
+  uint16_t best, diff;
+
+  if (i2cp->config != NULL)
+    divisor = KINETIS_SYSCLK_FREQUENCY / i2cp->config->clock;
+  else
+    divisor = KINETIS_SYSCLK_FREQUENCY / 100000;
+
+  best = ~0;
+  index = 0;
+  /* Tries to find the SCL clock which is the closest
+   * approximation to the clock passed in config. To
+   * stay on the safe side, only values that generate
+   * lower frequency are used.
+   */
+  for (i = 0; i < length; i++) {
+    if (icr_table[i] >= divisor) {
+      diff = icr_table[i] - divisor;
+      if (diff < best) {
+        best = diff;
+        index = i;
+      }
+    }
+  }
+
+  i2cp->i2c->F = index;
+  uint16_t t = 512*1*scl_stop_table[index];
+  i2cp->i2c->SLTH = t>>8;
+  i2cp->i2c->SLTL = t&0xFF;
+}
+
+/**
+ * @brief   Common IRQ handler.
+ * @note    Tries hard to clear all the pending interrupt sources, we don't
+ *          want to go through the whole ISR and have another interrupt soon
+ *          after.
+ *
+ * @param[in] i2cp         pointer to an I2CDriver
+ */
+
+
+#if 0
+#define cb(x) do { \
+    chSysLockFromISR(); \
+    x; \
+    chSysUnlockFromISR(); \
+  } while(0)
+#else
+  #define cb(x) x
+#endif
+
+
+static void serve_interrupt(I2CSlaveDriver *i2cp) {
+
+  I2C_TypeDef *i2c = i2cp->i2c;
+
+  uint8_t i2cS = i2c->S;
+  uint8_t i2cSMB = i2c->SMB;
+  uint8_t i2cC1 = i2c->C1&(~(I2Cx_C1_TXAK|I2Cx_C1_TX));// Default to Ack, RxMode
+  uint8_t c=0;
+  i2cp->errors = I2C_SLAVE_NO_ERROR;
+  if((i2cSMB & (I2Cx_SMB_SHTF2|I2Cx_SMB_SHTF2IE)) ==
+                                              (I2Cx_SMB_SHTF2|I2Cx_SMB_SHTF2IE))
+  {
+    i2cSMB &= ~I2Cx_SMB_SHTF2IE;
+    i2c->SMB = i2cSMB | I2Cx_SMB_SHTF2 | I2Cx_SMB_SLTF;
+    i2c->S = i2cS | I2Cx_S_IICIF;
+    i2cp->state = I2C_SLAVE_READY;
+      if (i2cp->rxidx && i2cp->config->rxend_cb != NULL)
+    cb(i2cp->config->rxend_cb(i2cp));
+  }
+  else if (!(i2cSMB & I2Cx_SMB_FACK) && (i2cS & I2Cx_S_TCF))
+  {
+    i2c->S = i2cS | I2Cx_S_IICIF;
+    if (i2cS & I2Cx_S_ARBL)
+    {
+      i2cp->errors |= I2C_SLAVE_ARBITRATION_LOST;
+      i2cp->state = I2C_SLAVE_READY;
+      i2c->S = i2cS | I2Cx_S_ARBL;
+    }
+    if(i2cS & I2Cx_S_IAAS)                                              // Start
+    {
+      if(i2cp->state == I2C_SLAVE_ACTIVE_RX)                          // ReStart
+      {
+        i2cSMB &= ~I2Cx_SMB_SHTF2IE;
+        // Callback I2C Stop for RX
+        if (i2cp->rxidx && i2cp->config->rxend_cb != NULL)
+        {
+          cb(i2cp->config->rxend_cb(i2cp));
+        }
+      }
+      if(i2cS & I2Cx_S_SRW)
+      {
+        i2cC1 |= I2Cx_C1_TX;
+        i2c->C1 = i2cC1;
+      }
+      c = i2c->D; // Read Address
+      if (i2cp->config->start_cb != NULL)
+      {
+        cb(i2cp->config->start_cb(i2cp,c));
+      }
+      if(i2cS & I2Cx_S_SRW) 		                                 // Start TX-ing
+      {
+        i2cp->state = I2C_SLAVE_ACTIVE_TX;
+        c = 0xFF; // Dummy byte
+        if(i2cp->txbuf != NULL && i2cp->txbytes)
+        {
+          c = i2cp->txbuf[0x00];
+          i2cp->txidx=0x1;
+        }
+        i2c->D = c; // Send first byte
+      }
+      else 	                    										             // Start RX-ing
+      {
+        i2cp->state = I2C_SLAVE_ACTIVE_RX;
+        i2c->SMB = i2cSMB | I2Cx_SMB_SHTF2IE;
+        i2cp->rxidx=0;
+      }
+      i2c->C1 = i2cC1;
+    }
+    else
+    {
+      if(i2cp->state == I2C_SLAVE_ACTIVE_TX) {			                   // TX-ing
+        if(!(i2cS & I2Cx_S_RXAK)) {		             // Master Ack-ed, Keep TX-ing
+          i2cp->state = I2C_SLAVE_ACTIVE_TX;
+          c = 0xFF;
+          if(i2cp->txbuf != NULL
+             && i2cp->txidx<i2cp->txbytes)    // TxBuffer not empty, Keep TX-ing
+          {
+            c = i2cp->txbuf[i2cp->txidx++];
+          }
+          i2c->D = c;                       // Send next byte
+          i2c->C1 = i2cC1|(I2Cx_C1_TX);     // TxMode
+        }
+        else
+        {											              // Stop TX-ing (Master just NAck-ed)
+          i2cp->state = I2C_SLAVE_READY;
+          i2c->C1 = i2cC1;                  // RxMode
+          (void)i2c->D;	// Dummy read
+          // Callback End TX
+          if (i2cp->txbytes && i2cp->txidx==i2cp->txbytes
+              && i2cp->config->txend_cb != NULL)
+          {
+            cb(i2cp->config->txend_cb(i2cp));
+          }
+        }
+      }
+      else									                                           // RX-ing
+      {
+        if(i2cp->rxbuf != NULL
+           && i2cp->rxidx < i2cp->rxbytes)       // Buffer not full, Keep RX-ing
+        {
+          i2cp->state = I2C_SLAVE_ACTIVE_RX;
+          if(i2cp->rxidx+1 >= i2cp->rxbytes)      // Stop RX-ing after this byte
+          {
+            i2cC1 |= I2Cx_C1_TXAK;          // RxMode, Nack
+          }
+          i2c->C1 = i2cC1;
+          i2cp->rxbuf[i2cp->rxidx++] = i2c->D;	// Read =)
+          // Callback Byte RX-ed
+        }
+        else                       // Buffer full, Stop RX-ing after this byte
+        {
+          i2cp->state = I2C_SLAVE_READY;
+          i2c->C1 = i2cC1|I2Cx_C1_TXAK;  // RxMode, Nack
+          (void)i2c->D;	// Dummy read
+        }
+      }
+    }
+  }
+  i2c->S = i2cS | I2Cx_S_IICIF;
+  if (i2cp->errors != I2C_SLAVE_NO_ERROR)
+    _i2c_slave_wakeup_error_isr(i2cp);
+  else if (i2cp->state == I2C_SLAVE_READY)
+    _i2c_slave_wakeup_isr(i2cp);
+}
+
+/*===========================================================================*/
+/* Driver interrupt handlers.                                                */
+/*===========================================================================*/
+
+#if KINETIS_I2C_SLAVE_USE_I2C0 || defined(__DOXYGEN__)
+
+PORT_IRQ_HANDLER(KINETIS_I2C0_IRQ_VECTOR) {
+
+  PORT_IRQ_PROLOGUE();
+  serve_interrupt(&I2CSD1);
+  PORT_IRQ_EPILOGUE();
+}
+
+#endif
+
+#if KINETIS_I2C_SLAVE_USE_I2C1 || defined(__DOXYGEN__)
+
+PORT_IRQ_HANDLER(KINETIS_I2C1_IRQ_VECTOR) {
+
+  PORT_IRQ_PROLOGUE();
+  serve_interrupt(&I2CSD2);
+  PORT_IRQ_EPILOGUE();
+}
+
+#endif
+/*===========================================================================*/
+/* Driver exported functions.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   Low level I2C driver initialization.
+ *
+ * @notapi
+ */
+void i2c_slave_lld_init(void) {
+
+#if KINETIS_I2C_SLAVE_USE_I2C0
+  i2cSlaveObjectInit(&I2CSD1);
+  I2CSD1.thread = NULL;
+  I2CSD1.i2c = I2C0;
+#endif /* PLATFORM_I2C_SLAVE_USE_I2C0 */
+#if KINETIS_I2C_SLAVE_USE_I2C1
+  i2cSlaveObjectInit(&I2CSD2);
+  I2CSD2.thread = NULL;
+  I2CSD2.i2c = I2C1;
+#endif /* PLATFORM_I2C_SLAVE_USE_I2C1 */
+}
+
+/**
+ * @brief   Configures and activates the I2C peripheral.
+ *
+ * @param[in] i2cp      pointer to t1he @p I2CDriver object
+ *
+ * @notapi
+ */
+
+void i2c_slave_lld_start(I2CSlaveDriver *i2cp) {
+
+  if (i2cp->state == I2C_SLAVE_STOP) {
+    /* Enables the peripheral.*/
+#if KINETIS_I2C_SLAVE_USE_I2C0
+    if (&I2CSD1 == i2cp) {
+      SIM->SCGC4 |= SIM_SCGC4_I2C0;
+      nvicEnableVector(I2C0_IRQn, KINETIS_I2C_SLAVE_I2C0_PRIORITY);
+    }
+#endif /* KINETIS_I2C_SLAVE_USE_I2C0 */
+#if KINETIS_I2C_SLAVE_USE_I2C1
+    if (&I2CSD2 == i2cp) {
+      SIM->SCGC4 |= SIM_SCGC4_I2C1;
+      nvicEnableVector(I2C0_IRQn, KINETIS_I2C_SLAVE_I2C1_PRIORITY);
+    }
+#endif /* KINETIS_I2C_SLAVE_USE_I2C1 */
+  }
+
+  slave_config_frequency(i2cp);
+
+  i2cp->i2c->A1 = I2Cx_A1_AD(i2cp->config->address);
+  i2cp->i2c->C1 = I2Cx_C1_IICEN | I2Cx_C1_IICIE;
+  i2cp->i2c->SMB = I2Cx_SMB_TCKSEL;
+
+  i2cp->txbytes=0;
+  i2cp->txsize=4;
+  i2cp->txbuf = i2c_txbuf;
+  i2cp->txidx=0;
+  i2cp->rxbytes=2;
+  i2cp->rxsize=4;
+  i2cp->rxbuf = i2c_rxbuf;
+  i2cp->rxidx=0;
+}
+
+/**
+ * @brief   Deactivates the I2C peripheral.
+ *
+ * @param[in] i2cp      pointer to the @p I2CDriver object
+ *
+ * @notapi
+ */
+void i2c_slave_lld_stop(I2CSlaveDriver *i2cp) {
+
+  if (i2cp->state != I2C_SLAVE_STOP) {
+    i2cp->i2c->C1 &= ~(I2Cx_C1_IICEN | I2Cx_C1_IICIE);
+
+    /* Disables the peripheral.*/
+#if KINETIS_I2C_SLAVE_USE_I2C0
+    if (&I2CSD1 == i2cp) {
+      SIM->SCGC4 &= ~SIM_SCGC4_I2C0;
+      nvicDisableVector(I2C0_IRQn);
+    }
+#endif /* KINETIS_I2C_SLAVE_USE_I2C0 */
+#if KINETIS_I2C_SLAVE_USE_I2C1
+    if (&I2CSD2 == i2cp) {
+      SIM->SCGC4 &= ~SIM_SCGC4_I2C1;
+      nvicDisableVector(I2C1_IRQn);
+    }
+#endif /* KINETIS_I2C_SLAVE_USE_I2C1 */
+  }
+}
+
+
+#endif /* HAL_USE_I2C_SLAVE */
+
+/** @} */
---
+++ custom/KINETIS/K20x/i2c_slave/i2c_slave_lld.h
@@ -0,0 +1,208 @@
+/*
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    i2c_slave_lld.h
+ * @brief   PLATFORM I2C subsystem low level driver header.
+ *
+ * @addtogroup I2C
+ * @{
+ */
+
+#ifndef _I2C_SLAVE_LLD_H_
+#define _I2C_SLAVE_LLD_H_
+
+#if HAL_USE_I2C_SLAVE || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+#define STATE_STOP    0x00
+#define STATE_SEND    0x01
+#define STATE_RECV    0x02
+#define STATE_DUMMY   0x03
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+/**
+ * @brief   I2C0 driver enable switch.
+ * @details If set to @p TRUE the support for I2C0 is included.
+ * @note    The default is @p FALSE.
+ */
+#if !defined(KINETIS_I2C_SLAVE_USE_I2C0) || defined(__DOXYGEN__)
+#define KINETIS_I2C_SLAVE_USE_I2C0                  FALSE
+#endif
+/**
+ * @brief   I2C1 driver enable switch.
+ * @details If set to @p TRUE the support for I2C is included.
+ * @note    The default is @p FALSE.
+ */
+#if !defined(KINETIS_I2C_SLAVE_USE_I2C1) || defined(__DOXYGEN__)
+#define KINETIS_I2C_SLAVE_USE_I2C                  FALSE
+#endif
+/** @} */
+
+/**
+ * @brief   I2C0 interrupt priority level setting.
+ */
+#if !defined(KINETIS_I2C_SLAVE_I2C0_PRIORITY) || defined(__DOXYGEN__)
+#define KINETIS_I2C_SLAVE_I2C0_PRIORITY        12
+#endif
+
+/**
+ * @brief   I2C1 interrupt priority level setting.
+ */
+#if !defined(KINETIS_I2C_SLAVE_I2C1_PRIORITY) || defined(__DOXYGEN__)
+#define KINETIS_I2C_SLAVE_I2C1_PRIORITY        12
+#endif
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   Type representing an I2C address.
+ */
+typedef uint16_t i2c_slave_addr_t;
+
+/**
+ * @brief   Type of I2C Driver condition flags.
+ */
+//~ typedef uint32_t i2c_slave_flags_t;
+
+typedef struct I2CSlaveDriver I2CSlaveDriver;
+
+typedef void (*i2c_simple_cb_t)(I2CSlaveDriver *i2cp);
+typedef void (*i2c_byte_cb_t)(I2CSlaveDriver *i2cp, uint8_t c);
+
+/**
+ * @brief   Type of I2C driver configuration structure.
+ * @note    Implementations may extend this structure to contain more,
+ *          architecture dependent, fields.
+ */
+typedef struct {
+  /* End of the mandatory fields.*/
+  uint32_t             clock;     /* @brief Clock to be used for the I2C bus. */
+
+  i2c_slave_addr_t     address;   /* @brief 7bits Address of the I2C Slave    */
+  i2c_byte_cb_t       start_cb;
+  //~ i2c_byte_cb_t        rxbyte_cb;
+  i2c_simple_cb_t       rxend_cb;
+  //~ i2c_byte_cb_t        rxbyte_cb;
+  i2c_simple_cb_t       txend_cb;
+  //~ i2c_byte_cb_t       error_cb;
+} I2CSlaveConfig;
+
+/**
+ * @brief   Structure representing an I2C driver.
+ */
+struct I2CSlaveDriver {
+  /**
+   * @brief   Driver state.
+   */
+  i2c_slave_state_t                state;
+  /**
+   * @brief   Current configuration data.
+   */
+  const I2CSlaveConfig           *config;
+  /**
+   * @brief   Error flags.
+   */
+  i2c_slave_error_flags_t         errors;
+#if I2C_USE_MUTUAL_EXCLUSION || defined(__DOXYGEN__)
+#if CH_CFG_USE_MUTEXES || defined(__DOXYGEN__)
+  /**
+   * @brief   Mutex protecting the bus.
+   */
+  mutex_t                   mutex;
+#elif CH_CFG_USE_SEMAPHORES
+  semaphore_t               semaphore;
+#endif
+#endif /* I2C_USE_MUTUAL_EXCLUSION */
+#if defined(I2C_SLAVE_DRIVER_EXT_FIELDS)
+  I2C_DRIVER_EXT_FIELDS
+#endif
+  /* @brief Thread waiting for I/O completion. */
+  thread_reference_t        thread;
+
+  /* End of the mandatory fields.*/
+
+  /* @brief Pointer to the buffer with data to send. */
+  uint8_t                   *txbuf;
+  /* @brief Number of bytes of data to send. */
+  size_t                    txbytes;
+  /* @brief Size of the buffer for sent data  */
+  size_t                    txsize;
+  /* @brief Current index in buffer when sending data. */
+  size_t                    txidx;
+  /* @brief Pointer to the buffer to put received data. */
+  uint8_t                   *rxbuf;
+  /* @brief Size of the buffer for received data */
+  size_t                    rxsize;
+  /* @brief Number of bytes of data to receive. */
+  size_t                    rxbytes;
+  /* @brief Current index in buffer when receiving data. */
+  size_t                    rxidx;
+  /* @brief Low-level register access. */
+  I2C_TypeDef               *i2c;
+};
+
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/**
+ * @brief   Get errors from I2C driver.
+ *
+ * @param[in] i2cp      pointer to the @p I2CDriver object
+ *
+ * @notapi
+ */
+#define i2c_slave_lld_get_errors(i2cp) ((i2cp)->errors)
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#if !defined(__DOXYGEN__)
+#if KINETIS_I2C_SLAVE_USE_I2C0
+extern I2CSlaveDriver I2CSD1;
+#endif
+#if KINETIS_I2C_SLAVE_USE_I2C1
+extern I2CSlaveDriver I2CSD2;
+#endif
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void i2c_slave_lld_init(void);
+  void i2c_slave_lld_start(I2CSlaveDriver *i2cp);
+  void i2c_slave_lld_stop(I2CSlaveDriver *i2cp);
+  //~ void  i2c_lld_slaveReceive(I2CSlaveDriver *i2cp, const I2CSlaveMsg *rxMsg);
+  //~ void  i2c_lld_slaveReply(I2CSlaveDriver *i2cp, const I2CSlaveMsg *replyMsg);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAL_USE_I2C_SLAVE */
+
+#endif /* _I2C_SLAVE_LLD_H_ */
+
+/** @} */
